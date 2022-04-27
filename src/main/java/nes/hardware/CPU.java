cbf3838b07b8205cd08e771a8af3e35ad9b1b714
chetan <tastyguava@gmail.com>
2022-04-27 10:35:06 +0530

    Made Some Changes to the CPU's status registers

--- src/main/java/nes/hardware/CPU.java
+++ src/main/java/nes/hardware/CPU.java
@@ -10,9 +10,36 @@ public class CPU {
   private static CPU cpu;
 
   private CPU() {
+
   }
 
-  public static CPU getInstance() {
+  private void setIrq(boolean value){
+    throw new UnsupportedOperationException();
+  }
+
+  private void powerUp() throws InvalidMemoryRangeException {
+    pc = 0x34;
+    a = x = y = 0;
+    sp = (byte) 0xFD;
+    writeToRam(0x4017, (byte) 0x00);
+    writeToRam(0x4015, (byte) 0x00);
+    for (int i = 0x4000; i <= 0x4013; i++)
+      writeToRam(i, (byte) 0x00);
+    //Todo: All 15 bits of noise channel LFSR = $0000
+  }
+
+  private void reset() {
+    sp = (byte) (sp - 3);
+    setIrq(false);
+    //Todo: APU mode in $4017 was unchanged
+    // APU was silenced ($4015 = 0)
+    // APU triangle phase is reset to 0 (i.e. outputs a value of 15, the first step of its waveform)
+    // APU DPCM output ANDed with 1 (upper 6 bits cleared)
+    // 2A03G: APU Frame Counter reset. (but 2A03letterless: APU frame counter retains old value) [6]
+
+  }
+
+  public static CPU getInstance() throws InvalidMemoryRangeException {
     if (cpu == null) {
       cpu = new CPU();
     }
@@ -26,80 +53,37 @@ public class CPU {
   // accumator and index registers
   byte a, x, y;
 
-  //registers
-  private int N; // Negative,
-  private int V; // Overflow
-  private int U; // unused
-  private int B; // break
-  private int D; // DecimalMode
-  private int I; // InterruptDisable
-  private int Z; // Zero,
-  private int C; // Carry private int
+  byte status_register;
 
-  enum Flag {
-    N,
-    V,
-    U,
-    B,
-    D,
-    I,
-    Z,
-    C,
+  private enum Flag{
+
+    C((byte)(1<<0)),
+    Z((byte)(1<<1)),
+    I((byte)(1<<2)),
+    D((byte)(1<<3)),
+    B((byte)(1<<4)),
+    U((byte)(1<<5)),
+    V((byte)(1<<6)),
+    N((byte)(1<<7));
+
+    public final byte value;
+
+    private Flag(byte value){
+      this.value = value;
+    }
+
   }
 
 
   private void setFlag(Flag flag, int value) {
-    switch (flag) {
-      case B:
-        B = value;
-        break;
-      case C:
-        C = value;
-        break;
-      case D:
-        D = value;
-        break;
-      case I:
-        I = value;
-        break;
-      case N:
-        N = value;
-        break;
-      case U:
-        U = value;
-        break;
-      case V:
-        V = value;
-        break;
-      case Z:
-        Z = value;
-        break;
-      default:
-        throw new IllegalStateException("Unexpected value: " + flag);
+    if(value == 1){
+      status_register |= flag.value;
     }
+    else status_register &= ~flag.value;
   }
 
-  public int getFlag(Flag flag) {
-    switch (flag) {
-      case B:
-        return B;
-      case C:
-        return C;
-      case D:
-        return D;
-      case I:
-        return I;
-      case N:
-        return N;
-      case U:
-        return U;
-      case V:
-        return V;
-      case Z:
-        return Z;
-      default:
-        throw new IllegalStateException("Unexpected value: " + flag);
-    }
+  public byte getFlag(Flag flag) {
+   return (byte) ((status_register & flag.value) > 0 ? 1:0);
   }
 
   public void push(byte data) {
@@ -113,24 +97,38 @@ public class CPU {
   public byte readFromRam(int address) throws InvalidMemoryRangeException {
     // range of Ram
     if (address >= 0x0000 && address <= 0x1FFF) {
-      return addressSpace[address & 0x0800];
+      return readFromRam(address & 0x0800);
+    } else if (address >= 0x2000 && address <= 0x3FFF) {
+      //Todo: Nes PPU Registers
+      //  address & 0x2007
+    } else if (address >= 0x4000 && address <= 0x4017) {
+      //Todo: NES APU and IO Registers
+      throw new UnsupportedOperationException();
+    } else if (address >= 0x4018 && address <= 0x401F) {
+      //Todo: APU and I/O functionality that is normally disabled
+      throw new UnsupportedOperationException();
+    } else if (address >= 0x4020 && address <= 0xFFFF) {
+      //Todo: Cartridge Space
+      // Mappers Here
+      throw new UnsupportedOperationException();
     }
+
     throw new InvalidMemoryRangeException();
   }
 
   public void writeToRam(int address, byte data) throws InvalidMemoryRangeException {
     // range of Ram
     if (address >= 0x0000 && address <= 0x1FFF) {
-      addressSpace[address & 0x0800] = data;
+      writeToRam(address & 0x0800, data);
     }
     throw new InvalidMemoryRangeException();
   }
 
-  private short absolute() {
+  private short absolute() throws InvalidMemoryRangeException {
     pc = (short) (pc + 1);
-    byte lowerByte = addressSpace[pc];
+    byte lowerByte = readFromRam(pc);
     pc = (short) (pc + 1);
-    byte upperByte = addressSpace[pc];
+    byte upperByte = readFromRam(pc);
 
     return (short) (lowerByte * 0x100 + upperByte);
   }
@@ -139,105 +137,105 @@ public class CPU {
     return a;
   }
 
-  private byte immediate() {
+  private byte immediate() throws InvalidMemoryRangeException {
     pc = (short) (pc + 1);
-    return addressSpace[pc];
+    return readFromRam(pc);
   }
 
   private void implied() {
   }
 
-  private byte indexedAbsoluteX() {
+  private byte indexedAbsoluteX() throws InvalidMemoryRangeException {
     pc = (short) (pc + 1);
-    byte lowerByte = addressSpace[pc];
+    byte lowerByte = readFromRam(pc);
     pc = (short) (pc + 1);
-    byte upperByte = addressSpace[pc];
+    byte upperByte = readFromRam(pc);
     short finalAddress = (short) ((upperByte << 8) | lowerByte);
-    return addressSpace[finalAddress + x];
+    return readFromRam(finalAddress + x);
   }
 
-  private byte indexedAbsoluteY() {
+  private byte indexedAbsoluteY() throws InvalidMemoryRangeException {
     pc = (short) (pc + 1);
-    byte lowerByte = addressSpace[pc];
+    byte lowerByte = readFromRam(pc);
     pc = (short) (pc + 1);
-    byte upperByte = addressSpace[pc];
+    byte upperByte = readFromRam(pc);
     byte finalAddress = (byte) ((upperByte << 8) | lowerByte);
-    return addressSpace[finalAddress + y];
+    return readFromRam(finalAddress + y);
   }
 
-  private byte indexedIndirect() {
+  private byte indexedIndirect() throws InvalidMemoryRangeException {
     pc = (short) (pc + 1);
-    byte nextByte = addressSpace[pc];
+    byte nextByte = readFromRam(pc);
     //Todo: Check this
     pc = (short) ((nextByte + x) & 0xFF);
-    byte lowerByte = addressSpace[pc];
+    byte lowerByte = readFromRam(pc);
     pc = (short) (pc + 1);
-    byte upperByte = addressSpace[pc];
-    return addressSpace[(byte) ((upperByte << 8) | lowerByte)];
+    byte upperByte = readFromRam(pc);
+    return readFromRam((byte) ((upperByte << 8) | lowerByte));
   }
 
-  private byte indirectIndexed() {
+  private byte indirectIndexed() throws InvalidMemoryRangeException {
     pc = (short) (pc + 1);
-    byte nextByte = addressSpace[pc];
+    byte nextByte = readFromRam(pc);
     byte targetAddressLowerByte = addressSpace[nextByte];
     byte targetAddressUpperByte = addressSpace[nextByte + 0x01];
     targetAddressLowerByte = (byte) (targetAddressLowerByte + y);
-    return addressSpace[(targetAddressUpperByte << 8) | targetAddressLowerByte];
+    return readFromRam((targetAddressUpperByte << 8) | targetAddressLowerByte);
   }
 
   private byte indexedZeroPageX() throws InvalidMemoryRangeException {
     pc = (short) (pc + 1);
-    byte nextByte = addressSpace[pc];
+    byte nextByte = readFromRam(pc);
     if (nextByte >= 0x0000 && nextByte <= 0x00ff) {
       pc = (short) (pc + 1);
       byte finalAddress = (byte) ((nextByte + x) & 0x00ff);
-      return addressSpace[finalAddress];
+      return readFromRam(finalAddress);
     }
     throw new InvalidMemoryRangeException();
   }
 
   private byte indexedZeroPageY() throws InvalidMemoryRangeException {
     pc = (short) (pc + 1);
-    byte nextByte = addressSpace[pc];
+    byte nextByte = readFromRam(pc);
     if (nextByte >= 0x0000 && nextByte <= 0x00ff) {
       pc = (short) (pc + 1);
       byte finalAddress = (byte) ((nextByte + y) & 0x00ff);
-      return addressSpace[finalAddress];
+      return readFromRam(finalAddress);
     }
     throw new InvalidMemoryRangeException();
   }
 
 
-  private short indirect() {
+  private short indirect() throws InvalidMemoryRangeException {
     pc = (short) (pc + 1);
-    byte lowerByte = addressSpace[pc];
+    byte lowerByte = readFromRam(pc);
     pc = (short) (pc + 1);
-    byte upperByte = addressSpace[pc];
+    byte upperByte = readFromRam(pc);
     return (byte) ((upperByte << 8) | lowerByte);
   }
 
 
-  private byte relative() {
+  private byte relative() throws InvalidMemoryRangeException {
     pc = (short) (pc + 1);
-    byte address = addressSpace[pc];
+    byte address = readFromRam(pc);
     address = (byte) (address + pc);
     pc = (short) (pc + 1);
-    return addressSpace[address];
+    return readFromRam(address);
   }
 
   private byte zeroPage() throws InvalidMemoryRangeException {
     pc = (short) (pc + 1);
-    byte nextByte = addressSpace[pc];
+    byte nextByte = readFromRam(pc);
     if (nextByte >= 0x0 && nextByte <= 0xf) {
       pc = (short) (pc + 1);
-      return addressSpace[nextByte];
+      return readFromRam(nextByte);
     }
     throw new InvalidMemoryRangeException();
   }
 
   //Instructions
   private void adc() {
-    short sum = (short) (a + fetchedValue + C);
+    short sum = (short) (a + fetchedValue + getFlag(Flag.C));
     if (sum > 255) setFlag(Flag.C, 1);
     else setFlag(Flag.C, 0);
     setFlag(Flag.Z, (sum & 0x00FF));
@@ -561,18 +559,18 @@ public class CPU {
     pc = (short) (pc + 1);
   }
 
-  private void sta() {
-    addressSpace[pc] = a;
+  private void sta() throws InvalidMemoryRangeException {
+    writeToRam(pc, a);
     pc = (short) (pc + 1);
   }
 
-  private void stx() {
-    addressSpace[pc] = x;
+  private void stx() throws InvalidMemoryRangeException {
+    writeToRam(pc, x);
     pc = (short) (pc + 1);
   }
 
-  private void sty() {
-    addressSpace[pc] = y;
+  private void sty() throws InvalidMemoryRangeException {
+    writeToRam(pc, y);
     pc = (short) (pc + 1);
   }
 
@@ -621,7 +619,7 @@ public class CPU {
   private short fetchedValue;
 
   void decode() throws InvalidOpcodeFound, InvalidMemoryRangeException {
-    byte hexValue = addressSpace[pc];
+    byte hexValue = readFromRam(pc);
     InstructionInfo instructionInfo = OpcodeDecoder.decode(hexValue);
     switch (instructionInfo.getAddressingMode()) {
       case Absolute:
